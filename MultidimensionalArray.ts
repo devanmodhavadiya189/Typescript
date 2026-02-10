935cc4ea32c45ba0f3d988cd049af21b0e041cd8
Devan Modhavadiya <modhavadiyadevan189@gmail.com>
2026-02-10 19:18:46 +0530

    Started Day 4

---
+++ MultidimensionalArray.ts
@@ -0,0 +1,12 @@
+// a multidimenstional array is an array that contains other arrays as its elements ,
+// multidimensional array can defined using the same notation as one-dimensional
+// But with nested squre brackets
+
+let sinngledim : number[]=[1,2,3];
+
+let multidim : number[][]=[[1,2,3],[4,5,6]];
+
+let tripledim : number[][][]=[[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12],[1,2]]];
+console.log(sinngledim);
+console.log(multidim);
+console.log(tripledim);
---
+++ Objects.ts
@@ -0,0 +1,5 @@
+// Objects
+//An Object in Ts is a strctured Datatype that represents a collection of properties , each with a key and an associated value
+// The Property of an object can have an specific types , and the object itself can  be annotated with a type,
+// often defined using an interface or a type alias, TS uses strctural typing , meaning that the shape of an object (its strcture   or 
+// proprties ) is what matters for type compatability
